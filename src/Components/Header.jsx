adb61c6df335def895ffa81a74fe1b7ea066d1a0
SOORYA.S <154416428+SSOORYA007@users.noreply.github.com>
2024-06-09 21:43:10 +0530

    Add files via upload

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Header=() =>{
+    return(
+        <div className="header">
+            <h1 className="title">Noes</h1>
+        </div>
+    )
+}
+export default Header
